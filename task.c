3894f2478f050240ebf551101cd54b04ee46528f
Abdelrhman Zaki <body15zaki@gmail.com>
2026-03-06 06:16:18 +0200

    modefied c file(task.c)

--- task.c
+++ task.c
@@ -6,5 +6,13 @@ int main()
     printf("Enter two numbers: ");
     scanf("%d %d",&a,&b);
     printf("Sum of %d and %d is %d\n",a,b,a+b);
+    if (a==b)
+    {
+        printf("The numbers are equal.\n");
+    }
+    else
+    {
+        printf("The numbers are not equal.\n");
+    }
     return 0;
 }
